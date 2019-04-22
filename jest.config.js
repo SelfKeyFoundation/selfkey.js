3aae65f38d71062788660e4eb9b3fb3113f1f024
Felipe Carlos Werlang <felipewer@gmail.com>
2019-04-22 20:54:40 +0800

    Clean dependencies and start new development setup.

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+	globalSetup: '<rootDir>/test/jest-setup.js',
+	globalTeardown: '<rootDir>/test/jest-teardown.js',
+	setupTestFrameworkScriptFile: '<rootDir>/test/jest-setup-test-framework.js',
+	testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+	modulePaths: ['<rootDir>/src'],
+	testMatch: ['**/src/**/*.spec.js', '**/src/**/*.steps.js'],
+	verbose: true,
+	testEnvironment: 'node',
+	testURL: 'http://localhost/',
+	collectCoverageFrom: ['src/{main,common}/**/*.js'],
+	coverageReporters: ['json', 'lcov', 'text'],
+	coveragePathIgnorePatterns: ['src/main/(seed|assets|migrations)'],
+	coverageDirectory: 'dist/coverage',
+	coverageThreshold: {}
+};
+module.exports = {
+	testPathIgnorePatterns: ['/node_modules/'],
+	modulePaths: ['<rootDir>/src'],
+	testMatch: ['**/src/**/*.spec.js'],
+	verbose: true,
+	testEnvironment: 'node',
+};
---
+++ src/auth/index.spec.js
@@ -0,0 +1,14 @@
+import { generateNonce } from './index'
+
+describe('Generate nonce', () => {
+    
+    it('correct length', async () => {
+        expect((await generateNonce()).length).toBe(88);
+    });
+
+	it('generates different values', async () => {
+        const nonce1 = await generateNonce();
+        const nonce2 = await generateNonce();
+		expect(nonce1 !== nonce2).toBe(true);
+    });
+});
