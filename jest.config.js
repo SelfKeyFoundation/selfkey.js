eba59733f02e9fc4b174022a7de97eb9eb7a1622
Maxim Kovalov <max.kovalov@gmail.com>
2019-04-23 12:59:42 +0300

    feat(cleanup): clean frontend configs

--- jest.config.js
+++ jest.config.js
@@ -2,22 +2,17 @@ module.exports = {
 	globalSetup: '<rootDir>/test/jest-setup.js',
 	globalTeardown: '<rootDir>/test/jest-teardown.js',
 	setupFilesAfterEnv: ['<rootDir>/test/jest-setup-test-framework.js'],
-	testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+	testPathIgnorePatterns: ['/node_modules/'],
 	modulePaths: ['<rootDir>/src'],
-	testMatch: ['**/src/**/*.spec.js', '**/src/**/*.steps.js'],
+	testMatch: ['**/src/**/*.spec.js'],
 	verbose: true,
 	testEnvironment: 'jsdom',
 	testURL: 'http://localhost/',
-	collectCoverageFrom: ['src/{main,common}/**/*.js'],
+	collectCoverageFrom: ['src/**/*.js'],
 	coverageReporters: ['json', 'lcov', 'text'],
-	coveragePathIgnorePatterns: ['src/main/(seed|assets|migrations)'],
 	coverageDirectory: 'dist/coverage',
 	coverageThreshold: {},
 	transform: {
 		'^.+\\.(js|jsx)$': 'babel-jest'
-	},
-	moduleNameMapper: {
-		'\\.(css|less)$': '<rootDir>/test/styleMock.js',
-		'\\.(png|gif|ttf|eot|svg)$': '<rootDir>/test/fileMock.js'
 	}
 };
