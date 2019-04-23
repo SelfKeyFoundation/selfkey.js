7560ce35e1c3989422cc29779e90cab14dce6ab4
Maxim Kovalov <max.kovalov@gmail.com>
2019-04-23 20:16:22 +0300

    feat(jwt): issue jwt token

--- src/jwt/index.js
+++ src/jwt/index.js
@@ -1,6 +1,11 @@
 import { promisify } from 'util';
 import jwt from 'jsonwebtoken';
 
+const JWT_ALGORITHMS = {
+	RSA: 'RS512',
+	HMAC: 'HS512'
+};
+
 const signJWT = promisify(jwt.sign);
 
 export const parseJWT = () => {};
@@ -9,21 +14,27 @@ export const validateJWT = () => {};
 
 export const issueJWT = (
 	subject,
-	algorithm,
+	requestedAlgorithm,
 	secret,
 	typ = 'JWT',
 	expiresIn = '1h',
 	additionalClaims = {}
 ) => {
 	const payload = {
-		typ,
-		...additionalClaims
+		...additionalClaims,
+		typ
 	};
-	if (algorithm === 'RSA') {
-		algorithm = 'RS512';
-	} else {
-		algorithm = 'HS512';
+
+	delete payload.exp;
+	delete payload.iat;
+	delete payload.sub;
+
+	const algorithm = JWT_ALGORITHMS[(requestedAlgorithm || '').toUpperCase()];
+
+	if (!algorithm) {
+		throw new Error(`Unknown algorithm ${requestedAlgorithm}`);
 	}
+
 	const options = {
 		algorithm,
 		subject,
