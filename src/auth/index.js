d5c93122f002027cc63760458292be8b1324aaf8
Maxim Kovalov <max.kovalov@gmail.com>
2019-04-25 18:16:44 +0300

    feat(auth): generate access token

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,1 +1,5 @@
-export { generateNonce, generateChallengeToken } from './generate-challenge-token';
+export { generateChallengeToken } from './generate-challenge-token';
+export { generateNonce } from './generate-nonce';
+export { generateAccessToken } from './generate-access-token';
+export { validateChallengeToken } from './validate-challenge-token';
+export { validateAccessToken } from './validate-access-token';
